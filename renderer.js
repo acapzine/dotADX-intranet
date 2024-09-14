1edcb82fb96ac1176dbf83046722473e1abb160f
acapzine <119457323+acapzine@users.noreply.github.com>
2024-09-14 17:14:23 -0400

    included in index.html

--- renderer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const searchButton = document.getElementById("search-button");
-  const searchInput = document.getElementById("search-input");
-
-  searchButton.addEventListener("click", () => {
-    const query = searchInput.value;
-    window.api.search(query);
-  });
-});
